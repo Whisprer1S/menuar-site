f5506d7419f60d4254854a8e0f96fa930fd45459
Whisprer1S <sabasaba2018@gmail.com>
2026-07-24 16:08:27 +0400

    Set up rebuild branch: framer-motion, tokens, fonts, NewSite shell

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import App from './App.jsx';
+import NewSite from './newsite';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <NewSite />
     <Analytics />
     <SpeedInsights />
   </React.StrictMode>,
